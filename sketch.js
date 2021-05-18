e488d2f4e83f7af28bdb95067168712111373e29
aakksshhatt <75571599+aakksshhatt@users.noreply.github.com>
2021-05-18 12:02:55 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+
+var engine, world;
+var drops = [];
+var rand;
+
+var maxDrops=100;
+
+
+
+
+
+function setup(){
+    engine = Engine.create();
+    world = engine.world;
+
+    createCanvas(400,700);
+    umbrella = new Umbrella(200,500);
+
+    //creating drops
+    if(frameCount % 150 === 0){
+
+        for(var i=0; i<maxDrops; i++){
+            drops.push(new createDrop(random(0,400), random(0,400)));
+        }
+
+    }
+    
+}
+
+function draw(){
+    Engine.update(engine);
+    background(0); 
+
+
+    
+        
+        
+
+    umbrella.display();
+
+    //displaying rain drops
+    for(var i = 0; i<maxDrops; i++){
+        drops[i].showDrop();
+        drops[i].updateY()
+    }
+    
+    drawSprites();
+}   
+
